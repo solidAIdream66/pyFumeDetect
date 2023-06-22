442d3b4e2dddf54c7a4f1166048b3441c9762415
solidAIdream66 <lidongshen@gmail.com>
2023-06-22 17:03:03 -0500

    using streamlit to demo truck track online

---
+++ trackTruck_streamlit.py
@@ -0,0 +1,33 @@
+import streamlit as st
+import cv2
+import tempfile
+from readTrafficVideo import playVideo
+
+
+if __name__ == "__main__":
+    st.title("Track truck on traffic videos using OpenCV")
+
+    source = st.selectbox('choose a traffic video file from', ('local', 'server'))
+
+    video_file = None
+    if source == 'local':
+        uploaded_file = st.file_uploader("choose a traffic video file", type=['mp4'])
+        if uploaded_file:
+            with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(uploaded_file.read())
+                video_file = temp_file.name
+    elif source == 'server':
+        video_file = 'Traffic Videos/test.mp4'
+
+    if video_file:
+        st_frame = st.empty()
+        showFrame = lambda frame: st_frame.image(frame[:, :, ::-1])
+        playVideo(video_file, showFrame)
+        # h_video = cv2.VideoCapture(video_file)
+
+        # st.write('codec', int(h_video.get(cv2.CAP_PROP_FOURCC)).to_bytes(4, byteorder=sys.byteorder).decode())
+        #
+        # h_video.release()
+        # exit()
+
+
